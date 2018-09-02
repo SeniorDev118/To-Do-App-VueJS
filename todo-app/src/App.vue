8ca0097da62f54cc900729b1ea645055f08f7d5b
SeniorDev118 <pewallin0527@gmail.com>
2018-09-02 19:04:49 +0300

    add items and using sweetalert in app

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -5,9 +5,34 @@
 </template>
 
 <script>
+import TodoList from '@/components/TodoList'
 export default {
-  name: 'App'
-}
+  name: 'App',
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+};
 </script>
 
 <style>
